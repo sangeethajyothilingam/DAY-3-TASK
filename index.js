b19f472bd468b33344a1fea32f59711a61446c93
sangeethajyothilingam <112925996+sangeethajyothilingam@users.noreply.github.com>
2022-09-21 22:09:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+    var info={
+    "basics": {
+      "name": "sangeetha jyothilingam",
+      "email": "sangeethajoe2007@gmail.com",
+      "phone": "9841223873",
+      "degree": "M.Sc.,M.Phil., Ph.D.,",
+      "location": {
+        "address": "N0.4, john vasu street, madhavaram",
+        "postalCode": "600051",
+        "city": "chennai",
+        "countryCode": "India",
+        "region": "Tamilnadu"
+      },
+      },
+    "work": [
+      {
+        "company": "Chettinad vidyashram",
+        "position": "Teacher",
+        "startDate": "1-6-2016",
+        "endDate": "31-4.2022",
+        "highlights": [
+          "worked as a academic coordinator, house mistress."
+        ]
+      }
+    ],
+    "education": [
+      {
+        "institution": "Bharathi Womens College, Chennai ",
+        "area": "PhD in Biochemistry (oncology) ",
+        "studyType": "Research",
+        "startDate": "2007 April ",
+        "endDate":  "2013 August",
+        "gpa": "Highly commented.",
+      }
+    ],
+    "awards": [
+      {
+        "title": "University rank holder",
+        "awarder": "state government",
+        "summary": "Received Madras University (State level) third Rank in B.Sc. Biochemistry."
+      }
+    ],
+    "publications": [
+      {
+        "name": ". Antioxdant status and radical scavenging activity of ganesh variety of the punica granatum rind extracts.",
+        "publisher": ". American journal of Pharmtech Research",
+        "releaseDate": "2012, vol 2(1): 377-388.",
+        "website": "www.ajptr.com",
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "basic",
+      }
+    ],
+    "languages": [
+      {
+        "language": "English, Tamil",
+        "fluency": "English, Tamil"
+      }
+    ],
+  }
+  console.log(info)
